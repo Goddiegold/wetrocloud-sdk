7f40418c9f19203b8baf630c22803f8059ee4351
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2025-03-19 07:45:46 +0100

    updated jest config.

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,26 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// export default {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+//   moduleFileExtensions: ['ts', 'js'],
+//   extensionsToTreatAsEsm: ['.ts'],
+//   globals: {
+//     'ts-jest': {
+//       useESM: true,
+//     },
+//   },
+// };
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm', // Use ESM preset
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js'],
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'cjs'],
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Fixes module resolution issue
   },
 };
